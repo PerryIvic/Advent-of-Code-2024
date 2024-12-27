54cd892e2ca258ea0ce5731b4c560f8bc6f663da
PerryIvic <perry.ivic@studerande.thegameassembly.se>
2024-12-27 09:17:40 +0100

    Added solution for puzzle 6 Part 1. Created simple Vector2 class

--- Source/AdventPuzzle6/Main.cpp
+++ Source/AdventPuzzle6/Main.cpp
@@ -3,12 +3,19 @@
 #include <vector>
 #include <algorithm>
 #include <unordered_map>
+#include "Vector2.h"
 
 #include "Print.h"
 
-using Grid = std::vector<std::vector<char>>;
+using Map = std::vector<std::vector<char>>;
 
-void ReadMap(Grid& aGrid, const std::string aPath)
+struct GuardData
+{
+	Math::Vector2 myPosition;
+	Math::Vector2 myDirection;
+};
+
+void ReadMap(Map& aMap, const std::string aPath)
 {
 	std::ifstream file(aPath);
 
@@ -21,18 +28,124 @@ void ReadMap(Grid& aGrid, const std::string aPath)
 	while (getline(file, line))
 	{
 		std::vector<char> row(line.begin(), line.end());
-		aGrid.push_back(row);
+		aMap.push_back(row);
 	}
 
 	file.close();
+
+	if (aMap.empty())
+	{
+		Debug::Print("Map is empty");
+	}
+}
+
+GuardData FindGuardData(const Map& aMap)
+{
+	for (int y = 0; y < aMap.size(); ++y)
+	{
+		for (int x = 0; x < aMap[y].size(); ++x)
+		{
+			char tile = aMap[y][x];
+
+			if (tile == '^')
+			{
+				GuardData spawnData;
+				spawnData.myDirection = { 0 , -1 };
+				spawnData.myPosition = { x, y };
+
+				return spawnData;
+			}
+		}
+	}
+}
+
+bool IsPositionWithinMap(const Math::Vector2 aPosition, const Map& aMap)
+{
+	if (aPosition.myY >= 0 && aPosition.myY < aMap.size())
+	{
+		if (aPosition.myX >= 0 && aPosition.myX < aMap[aPosition.myY].size())
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+void GuardTraversesMap(Map& aMap)
+{
+	GuardData guard = FindGuardData(aMap);
+
+	bool isGuardOutOfBounds = false;
+	while (!isGuardOutOfBounds)
+	{
+		Math::Vector2 guardPos = guard.myPosition;
+		aMap[guardPos.myY][guardPos.myX] = 'X';
+
+		Math::Vector2 aheadPos = guard.myPosition + guard.myDirection;
+		if (IsPositionWithinMap(aheadPos, aMap))
+		{
+			char tile = aMap[aheadPos.myY][aheadPos.myX];
+			if (tile == '#')
+			{
+				guard.myDirection = { -guard.myDirection.myY, guard.myDirection.myX };
+			}
+			else
+			{
+				guard.myPosition += guard.myDirection;
+			}
+		}
+		else
+		{
+			isGuardOutOfBounds = true;
+		}
+	}
+}
+
+int CountTraversedTiles(const Map& aMap)
+{
+	int numExploredTiles = 0;
+	for (int y = 0; y < aMap.size(); ++y)
+	{
+		for (int x = 0; x < aMap[y].size(); ++x)
+		{
+			const char tile = aMap[y][x];
+			if (tile == 'X')
+				++numExploredTiles;
+		}
+	}
+
+	return numExploredTiles;
+}
+
+void PrintMap(const Map& aMap)
+{
+	for (int y = 0; y < aMap.size(); ++y)
+	{
+		std::string line;
+		for (int x = 0; x < aMap[y].size(); ++x)
+		{
+			line += aMap[y][x];
+		}
+		Debug::Print(line);
+	}
 }
 
 int main()
 {
-	const std::string mapPath = "../../Inputs/puzzle_06_input_test";
+	const std::string mapPath = "../../Inputs/puzzle_06_input.txt";
+	//const std::string mapPath = "../../Inputs/puzzle_06_input_test.txt";
 
-	Grid grid;
-	ReadMap(grid, mapPath);
+	Map map;
+	ReadMap(map, mapPath);
+
+	GuardTraversesMap(map);
+
+	PrintMap(map);
+
+	const int result = CountTraversedTiles(map);
+
+	Debug::PrintInt(result);
 
 	return 0;
 }
---
+++ Source/HelperTools/Vector2.h
@@ -0,0 +1,50 @@
+#pragma once
+
+namespace Math
+{
+	class Vector2
+	{
+	public:
+		Vector2()
+			: myX(0)
+			, myY(0)
+		{
+		}
+
+		Vector2(int anX, int aY)
+			: myX(anX)
+			, myY(aY)
+		{
+		}
+
+		~Vector2() = default;
+
+		Vector2& operator=(const Vector2& aRhs) = default;
+
+	public:
+		int myX;
+		int myY;
+	};
+
+	Vector2 operator+(const Vector2& aLhs, const Vector2& aRhs)
+	{
+		return Vector2(aLhs.myX + aRhs.myX, aLhs.myY + aRhs.myY);
+	}
+
+	Vector2 operator-(const Vector2& aLhs, const Vector2& aRhs)
+	{
+		return Vector2(aLhs.myX - aRhs.myX, aLhs.myY - aRhs.myY);
+	}
+
+	void operator+=(Vector2& aLhs, const Vector2& aRhs)
+	{
+		aLhs.myX += aRhs.myX;
+		aLhs.myY += aRhs.myY;
+	}
+
+	void operator-=(Vector2& aLhs, const Vector2& aRhs)
+	{
+		aLhs.myX -= aRhs.myX;
+		aLhs.myY -= aRhs.myY;
+	}
+}
