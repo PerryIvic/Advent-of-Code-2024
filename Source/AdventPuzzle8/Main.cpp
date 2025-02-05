4f84bcf41ba692d8af4af189a57be444f0f2ee0d
Perry <perry.ivic@hotmail.com>
2025-02-05 21:25:20 +0100

    Puzzle 8 part 1 solution done + added clang format

--- Source/AdventPuzzle8/Main.cpp
+++ Source/AdventPuzzle8/Main.cpp
@@ -1,6 +1,173 @@
 #include "Print.h"
+#include <Vector2.h>
+#include <fstream>
+#include <string>
+#include <utility>
+#include <vector>
+
+struct Tile
+{
+    Tile() : myID('?'), myHasAntiNode(false) {};
+
+    char myID;
+    bool myHasAntiNode;
+};
+
+using Grid = std::vector<std::vector<Tile>>;
+
+void PrintGrid(const Grid& aGrid)
+{
+    for (uint32_t y = 0; y < aGrid.size(); ++y)
+    {
+        for (uint32_t x = 0; x < aGrid[y].size(); ++x)
+        {
+            std::cout << aGrid[y][x].myID;
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << std::endl;
+}
+
+void ReadFile(Grid& outGrid, const std::string aPath)
+{
+    std::ifstream file(aPath);
+
+    if (!file.is_open())
+    {
+        Debug::Print("Failed to open " + aPath);
+    }
+
+    std::string line;
+    while (getline(file, line))
+    {
+        Tile tile;
+        std::vector<Tile> row;
+        for (char c : line)
+        {
+            tile.myID = c;
+            row.push_back(tile);
+        }
+
+        outGrid.push_back(row);
+    }
+
+    file.close();
+
+    if (outGrid.empty())
+    {
+        Debug::Print("outGrid is empty");
+    }
+}
+
+bool IsWithinGrid(const Grid& aGrid, const Math::Vector2 aPosition)
+{
+    if (aPosition.myY >= 0 && aPosition.myY < aGrid.size())
+    {
+        if (aPosition.myX >= 0 && aPosition.myX < aGrid[aPosition.myY].size())
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool IsSatelite(const char anId)
+{
+    return anId != '.' && anId != '#';
+}
+
+void RegisterAntiNodes(Grid& aGrid)
+{
+    for (int innerY = 0; innerY < aGrid.size(); ++innerY)
+    {
+        for (int innerX = 0; innerX < aGrid[innerY].size(); ++innerX)
+        {
+            const Tile firstTile = aGrid[innerY][innerX];
+            if (!IsSatelite(firstTile.myID))
+                continue;
+
+            for (int outerY = 0; outerY < aGrid.size(); ++outerY)
+            {
+                for (int outerX = 0; outerX < aGrid[outerY].size(); ++outerX)
+                {
+                    const Tile secondTile = aGrid[outerY][outerX];
+
+                    const Math::Vector2 innerPos = Math::Vector2(innerX, innerY);
+                    const Math::Vector2 outerPos = Math::Vector2(outerX, outerY);
+                    if (!IsSatelite(secondTile.myID) || firstTile.myID != secondTile.myID || innerPos == outerPos)
+                        continue;
+
+                    const Math::Vector2 diff = (outerPos - innerPos);
+
+                    const Math::Vector2 antiNodePosInner = innerPos - diff;
+                    const Math::Vector2 antiNodePosOuter = outerPos + diff;
+
+                    if (IsWithinGrid(aGrid, antiNodePosInner))
+                    {
+                        Tile& n1 = aGrid[antiNodePosInner.myY][antiNodePosInner.myX];
+                        if (!IsSatelite(n1.myID))
+                        {
+                            n1.myID = '#';
+                        }
+                        n1.myHasAntiNode = true;
+                    }
+
+                    if (IsWithinGrid(aGrid, antiNodePosOuter))
+                    {
+                        Tile& n2 = aGrid[antiNodePosOuter.myY][antiNodePosOuter.myX];
+                        if (!IsSatelite(n2.myID))
+                        {
+                            n2.myID = '#';
+                        }
+                        n2.myHasAntiNode = true;
+                    }
+                }
+            }
+        }
+    }
+}
+
+int GetUniqueAntiNodeAmount(const Grid& aGrid)
+{
+    int antiNodeAmount = 0;
+    for (int innerY = 0; innerY < aGrid.size(); ++innerY)
+    {
+        for (int innerX = 0; innerX < aGrid[innerY].size(); ++innerX)
+        {
+            const Tile firstTile = aGrid[innerY][innerX];
+            if (firstTile.myHasAntiNode)
+            {
+                ++antiNodeAmount;
+            }
+        }
+    }
+
+    return antiNodeAmount;
+}
+
+int Part1(const std::string aFilePath)
+{
+    Grid grid;
+    ReadFile(grid, aFilePath);
+
+    RegisterAntiNodes(grid);
+
+    PrintGrid(grid);
+
+    return GetUniqueAntiNodeAmount(grid);
+}
 
 int main()
 {
-	return 0;
+    const std::string filePath = "../../Inputs/puzzle_08_input.txt";
+    // const std::string filePath = "../../Inputs/puzzle_08_test_input_01.txt";
+    // const std::string filePath = "../../Inputs/puzzle_08_test_input_02.txt";
+
+    const int resultPart1 = Part1(filePath);
+
+    Debug::PrintInt(resultPart1);
+
+    return 0;
 }
--- Source/HelperTools/Vector2.h
+++ Source/HelperTools/Vector2.h
@@ -2,60 +2,116 @@
 
 namespace Math
 {
-	class Vector2
-	{
-	public:
-		Vector2()
-			: myX(0)
-			, myY(0)
-		{
-		}
+class Vector2
+{
+  public:
+    Vector2() : myX(0), myY(0)
+    {
+    }
 
-		Vector2(int anX, int aY)
-			: myX(anX)
-			, myY(aY)
-		{
-		}
+    Vector2(int anX, int aY) : myX(anX), myY(aY)
+    {
+    }
 
-		~Vector2() = default;
+    ~Vector2() = default;
 
-		Vector2& operator=(const Vector2& aRhs) = default;
+    Vector2& operator=(const Vector2& aRhs) = default;
 
-	public:
-		int myX;
-		int myY;
-	};
+  public:
+    int myX;
+    int myY;
+};
 
-	Vector2 operator+(const Vector2& aLhs, const Vector2& aRhs)
-	{
-		return Vector2(aLhs.myX + aRhs.myX, aLhs.myY + aRhs.myY);
-	}
-
-	Vector2 operator-(const Vector2& aLhs, const Vector2& aRhs)
-	{
-		return Vector2(aLhs.myX - aRhs.myX, aLhs.myY - aRhs.myY);
-	}
-
-	void operator+=(Vector2& aLhs, const Vector2& aRhs)
-	{
-		aLhs.myX += aRhs.myX;
-		aLhs.myY += aRhs.myY;
-	}
-
-	void operator-=(Vector2& aLhs, const Vector2& aRhs)
-	{
-		aLhs.myX -= aRhs.myX;
-		aLhs.myY -= aRhs.myY;
-	}
-
-	bool operator==(const Vector2& aLhs, const Vector2& aRhs)
-	{
-		if (aLhs.myX == aRhs.myX &&
-			aLhs.myY == aRhs.myY)
-		{
-			return true;
-		}
-
-		return false;
-	}
+// Vector x Vector Operations
+Vector2 operator+(const Vector2& aLhs, const Vector2& aRhs)
+{
+    return Vector2(aLhs.myX + aRhs.myX, aLhs.myY + aRhs.myY);
 }
+
+Vector2 operator-(const Vector2& aLhs, const Vector2& aRhs)
+{
+    return Vector2(aLhs.myX - aRhs.myX, aLhs.myY - aRhs.myY);
+}
+
+void operator+=(Vector2& aLhs, const Vector2& aRhs)
+{
+    aLhs.myX += aRhs.myX;
+    aLhs.myY += aRhs.myY;
+}
+
+void operator-=(Vector2& aLhs, const Vector2& aRhs)
+{
+    aLhs.myX -= aRhs.myX;
+    aLhs.myY -= aRhs.myY;
+}
+
+bool operator==(const Vector2& aLhs, const Vector2& aRhs)
+{
+    if (aLhs.myX == aRhs.myX && aLhs.myY == aRhs.myY)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+// Vector x Scalar Operations
+Vector2 operator/(const Vector2& aVector, const int& aScalar)
+{
+    return Vector2(aVector.myX / aScalar, aVector.myY / aScalar);
+}
+
+Vector2 operator*(const Vector2& aVector, const int& aScalar)
+{
+    return Vector2(aVector.myX * aScalar, aVector.myY * aScalar);
+}
+
+Vector2 operator+(const Vector2& aVector, const int& aScalar)
+{
+    return Vector2(aVector.myX + aScalar, aVector.myY + aScalar);
+}
+
+Vector2 operator-(const Vector2& aVector, const int& aScalar)
+{
+    return Vector2(aVector.myX - aScalar, aVector.myY - aScalar);
+}
+
+void operator/=(Vector2& aVector, const int& aScalar)
+{
+    aVector.myX /= aScalar;
+    aVector.myY /= aScalar;
+}
+
+void operator*=(Vector2& aVector, const int& aScalar)
+{
+    aVector.myX *= aScalar;
+    aVector.myY *= aScalar;
+}
+
+void operator+=(Vector2& aVector, const int& aScalar)
+{
+    aVector.myX += aScalar;
+    aVector.myY += aScalar;
+}
+
+void operator-=(Vector2& aVector, const int& aScalar)
+{
+    aVector.myX -= aScalar;
+    aVector.myY -= aScalar;
+}
+
+} // namespace Math
+
+namespace std
+{
+template <> struct hash<Math::Vector2>
+{
+    std::size_t operator()(Math::Vector2 const& v) const noexcept
+    {
+        std::size_t x = std::hash<int>{}(v.myX);
+        std::size_t y = std::hash<int>{}(v.myY);
+
+        return x ^ y;
+    }
+};
+} // namespace std
